c0673f580469081b2487678f8f7922a75e05ec81
Stelarian Iftimie <stelarian.iftimie@certussoftware.ro>
2020-09-06 02:09:51 +0300

    Imposed account state restriction for user actions ViewInvoiceSheet and ViewInvoiceRecord.

--- base/ewallet.py
+++ base/ewallet.py
@@ -2590,6 +2590,18 @@ class EWallet(Base):
     [ NOTES ]: Enviroment checks for proper action execution are performed here.
     '''
 
+    def handle_user_action_view_invoice_list(self, **kwargs):
+        log.debug('')
+        check = self.check_user_logged_in()
+        return self.warning_user_not_logged_in(check, kwargs) if not check \
+            else self.action_view_invoice_list(**kwargs)
+
+    def handle_user_action_view_invoice_record(self, **kwargs):
+        log.debug('')
+        check = self.check_user_logged_in()
+        return self.warning_user_not_logged_in(check, kwargs) if not check \
+            else self.action_view_invoice_record(**kwargs)
+
     def handle_user_action_view_account(self, **kwargs):
         log.debug('')
         check = self.check_user_logged_in()
@@ -2805,14 +2817,6 @@ class EWallet(Base):
     def handle_user_action_view_logout_records(self, **kwargs):
         log.debug('')
         return self.action_view_logout_records(**kwargs)
-
-    def handle_user_action_view_invoice_list(self, **kwargs):
-        log.debug('')
-        return self.action_view_invoice_list(**kwargs)
-
-    def handle_user_action_view_invoice_record(self, **kwargs):
-        log.debug('')
-        return self.action_view_invoice_record(**kwargs)
 
     def handle_user_action_reset_alias(self, **kwargs):
         log.debug('')
