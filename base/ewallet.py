d677033af5e53c4fd85e689bdde2e958a38767df
Stelarian Iftimie <stelarian.iftimie@certussoftware.ro>
2020-09-06 02:06:00 +0300

    Imposed account state restriction for user actions ViewTimeSheet and ViewTimeRecord.

--- base/ewallet.py
+++ base/ewallet.py
@@ -2590,6 +2590,18 @@ class EWallet(Base):
     [ NOTES ]: Enviroment checks for proper action execution are performed here.
     '''
 
+    def handle_user_action_view_time_list(self, **kwargs):
+        log.debug('')
+        check = self.check_user_logged_in()
+        return self.warning_user_not_logged_in(check, kwargs) if not check \
+            else self.action_view_time_list(**kwargs)
+
+    def handle_user_action_view_time_record(self, **kwargs):
+        log.debug('')
+        check = self.check_user_logged_in()
+        return self.warning_user_not_logged_in(check, kwargs) if not check \
+            else self.action_view_time_record(**kwargs)
+
     def handle_user_action_view_transfer_list(self, **kwargs):
         log.debug('')
         check = self.check_user_logged_in()
@@ -2771,14 +2783,6 @@ class EWallet(Base):
     def handle_user_action_view_conversion_record(self, **kwargs):
         log.debug('')
         return self.action_view_conversion_record(**kwargs)
-
-    def handle_user_action_view_time_list(self, **kwargs):
-        log.debug('')
-        return self.action_view_time_list(**kwargs)
-
-    def handle_user_action_view_time_record(self, **kwargs):
-        log.debug('')
-        return self.action_view_time_record(**kwargs)
 
     def handle_user_action_view_invoice_list(self, **kwargs):
         log.debug('')
