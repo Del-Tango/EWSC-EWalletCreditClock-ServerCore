41848d148acf181b95e52f3459fb26cef8c4d30a
Stelarian Iftimie <stelarian.iftimie@certussoftware.ro>
2020-09-06 01:59:32 +0300

    Imposed account state restriction for user actions ViewTransferSheet and ViewTransferRecord.

--- base/ewallet.py
+++ base/ewallet.py
@@ -2590,6 +2590,18 @@ class EWallet(Base):
     [ NOTES ]: Enviroment checks for proper action execution are performed here.
     '''
 
+    def handle_user_action_view_transfer_list(self, **kwargs):
+        log.debug('')
+        check = self.check_user_logged_in()
+        return self.warning_user_not_logged_in(check, kwargs) if not check \
+            else self.action_view_transfer_list(**kwargs)
+
+    def handle_user_action_view_transfer_record(self, **kwargs):
+        log.debug('')
+        check = self.check_user_logged_in()
+        return self.warning_user_not_logged_in(check, kwargs) if not check \
+            else self.action_view_transfer_record(**kwargs)
+
     def handle_user_action_view_contact_list(self, **kwargs):
         log.debug('')
         check = self.check_user_logged_in()
@@ -2767,14 +2779,6 @@ class EWallet(Base):
     def handle_user_action_view_time_record(self, **kwargs):
         log.debug('')
         return self.action_view_time_record(**kwargs)
-
-    def handle_user_action_view_transfer_list(self, **kwargs):
-        log.debug('')
-        return self.action_view_transfer_list(**kwargs)
-
-    def handle_user_action_view_transfer_record(self, **kwargs):
-        log.debug('')
-        return self.action_view_transfer_record(**kwargs)
 
     def handle_user_action_view_invoice_list(self, **kwargs):
         log.debug('')
