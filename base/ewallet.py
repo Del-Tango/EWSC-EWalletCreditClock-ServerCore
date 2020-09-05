65a801c30789d4f79724535543ca30390f045765
Stelarian Iftimie <stelarian.iftimie@certussoftware.ro>
2020-09-06 01:45:02 +0300

    Imposed account state restriction for user actions ConvertCredits2Clock and ConvertClock2Credits.

--- base/ewallet.py
+++ base/ewallet.py
@@ -2590,6 +2590,18 @@ class EWallet(Base):
     [ NOTES ]: User account state checks are performed here.
     '''
 
+    def handle_user_action_create_new_conversion_credits_to_clock(self, **kwargs):
+        log.debug('')
+        check = self.check_user_logged_in()
+        return self.warning_user_not_logged_in(check, kwargs) if not check \
+            else self.action_create_new_conversion_credits_to_clock(**kwargs)
+
+    def handle_user_action_create_new_conversion_clock_to_credits(self, **kwargs):
+        log.debug('')
+        check = self.check_user_logged_in()
+        return self.warning_user_not_logged_in(check, kwargs) if not check \
+            else self.action_create_new_conversion_clock_to_credits(**kwargs)
+
     def handle_user_action_create_new_transfer_type_pay(self, **kwargs):
         log.debug('')
         check = self.check_user_logged_in()
@@ -2823,14 +2835,6 @@ class EWallet(Base):
     def handle_user_action_create_new_contact_record(self, **kwargs):
         log.debug('')
         return self.action_create_new_contact_record(**kwargs)
-
-    def handle_user_action_create_new_conversion_credits_to_clock(self, **kwargs):
-        log.debug('')
-        return self.action_create_new_conversion_credits_to_clock(**kwargs)
-
-    def handle_user_action_create_new_conversion_clock_to_credits(self, **kwargs):
-        log.debug('')
-        return self.action_create_new_conversion_clock_to_credits(**kwargs)
 
     def handle_user_action_create_new_transfer_type_transfer(self, **kwargs):
         log.debug('')
